f026d510a7f3b61ce421670d9db4d1c856b2e8f4
kripesh mainali <139145250+kripeshAlt@users.noreply.github.com>
2024-12-02 23:20:50 +0545

    Delete startup.sh

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/bin/Xvfb :1 -screen 0 1024x768x16 &
-/usr/bin/x11vnc -display :1 -nopw -forever -rfbport 5901 &
-/opt/noVNC/utils/launch.sh --vnc localhost:5901 --listen 6080 &
-exec "$@"
